8f1663d3421181f52f28a7f2e9f164c34b42fa54
OmidOmar <a.omidomari@gmail.com>
2022-11-22 17:23:03 +0000

    level 2 completed

--- challenge/src/App.js
+++ challenge/src/App.js
@@ -1,21 +1,44 @@
 import "./App.css";
 import babyNames from "./BabyNamesData.json";
+import { useState } from "react";
 
 function App() {
+  const [data, dataSet] = useState(
+    babyNames.sort((a, b) => (a.name > b.name ? 1 : -1))
+  );
+  const search = () => {
+    const filteredData = babyNames.filter((x) => {
+      return x.name
+        .toLowerCase()
+        .indexOf(document.getElementById("searchInput").value.toLowerCase()) >
+        -1
+        ? x
+        : "";
+    });
+    dataSet(filteredData);
+    console.log(filteredData);
+  };
+
   return (
     <div className="App">
+      <div className="form-group">
+        <input
+          type="text"
+          className="form-control"
+          id="searchInput"
+          onInput={search}
+        ></input>
+      </div>
       <div className="nameContainer">
         <div>
-          {babyNames
-            .sort((a, b) => (a.name > b.name ? 1 : -1))
-            .map((x, i) => {
-              let color = (x.sex==='m')?'info':'danger'
-              return (
-                <p key={i} className={`name bg-${color} m-2 lead align-middle`}>
-                  {x.name}
-                </p>
-              );
-            })}
+          {data.map((x, i) => {
+            let color = x.sex === "m" ? "info" : "danger";
+            return (
+              <p key={i} className={`name bg-${color} m-2 lead align-middle`}>
+                {x.name}
+              </p>
+            );
+          })}
         </div>
       </div>
     </div>
